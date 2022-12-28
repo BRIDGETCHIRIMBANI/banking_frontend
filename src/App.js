77ce271282856992b083436aa575e2e2a0000cf9
BRIDGETCHIRIMBANI <bridgetchirimbani22@gmail.com>
2022-12-28 11:29:31 +0400

    add login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import login from "./login.html";
+import Login from "./Login";
 
 function App() {
   return (
-    <div className="margin-left: 5%">
-      <h2>Crown Bank Limited</h2>
-      <p>We can do this guys, I appreciate y'all!!</p>
+    <div>
+      <Login/>
     </div>
   );
 }
